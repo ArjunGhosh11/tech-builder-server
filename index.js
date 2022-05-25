047a1a4356b0ae7ff070baf221d8decd19f8ceeb
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-05-25 15:10:30 +0600

    admin api addded with authorization

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ async function run() {
         const userCollection = client.db('tech-build').collection('users');
         const reviewCollection = client.db('tech-build').collection('reviews');
         const orderCollection = client.db('tech-build').collection('orders');
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
+
         //FOR PARTS
         app.get('/parts', async (req, res) => {
             const query = {};
@@ -69,10 +81,22 @@ async function run() {
         });
 
         //FOR USERS
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        });
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
-            const filter = { email: email, "role": "user" };
+            const filter = { email: email };
             const options = { upsert: true };
             const updateDoc = {
                 $set: user,
@@ -81,6 +105,34 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
         });
+        // app.put('/user/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id)
+        //     const user = req.body;
+        //     const options = { upsert: true };
+        //     const filter = { _id: ObjectId(id) };
+        //     const updatedDoc = {
+        //         $set: user,
+        //     }
+        //     const updatedUser = await userCollection.updateOne(filter, updatedDoc, options);
+        //     return res.send({ success: true, updatedUser });
+        // })
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+        //FOR ADMIN
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
     }
     finally {
 
