9464eaf1c425d19dc0b5a213a875e05adc86e11a
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-05-25 22:53:12 +0600

    Server is set up

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +39,7 @@ async function run() {
         const userCollection = client.db('tech-build').collection('users');
         const reviewCollection = client.db('tech-build').collection('reviews');
         const orderCollection = client.db('tech-build').collection('orders');
+        const paymentCollection = client.db('tech-build').collection('payments');
 
         const verifyAdmin = async (req, res, next) => {
             const requester = req.decoded.email;
@@ -49,6 +51,18 @@ async function run() {
                 res.status(403).send({ message: 'forbidden' });
             }
         }
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.totalCost;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
         //FOR PARTS
         app.get('/parts', async (req, res) => {
@@ -98,6 +112,29 @@ async function run() {
             const orders = await orderCollection.find(query).toArray();
             return res.send(orders);
         });
+
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await orderCollection.findOne(query);
+            res.send(order);
+        })
+
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paymentStatus: "paid",
+                    transactionId: payment.transactionId
+                }
+            }
+
+            const result = await paymentCollection.insertOne(payment);
+            const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+            res.send(updatedOrder);
+        })
         //FOR REVIEWS
         app.post('/reviews', async (req, res) => {
             const review = req.body;
@@ -124,6 +161,30 @@ async function run() {
             const user = await userCollection.findOne(query);
             res.send(user);
         });
+
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        })
+
+        app.patch('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    name: user.name,
+                    location: user.location,
+                    phone: user.phone,
+                    social: user.social
+                }
+            }
+            const updatedUser = await userCollection.updateOne(filter, updatedDoc);
+            return res.send({ success: true, updatedUser });
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
